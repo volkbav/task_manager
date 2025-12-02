929ca5d47ce319577c4ec89f002dd71b66aab8ba
volkbav <berezov86@gmail.com>
2025-12-02 21:06:52 +0300

    add logout btn

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from task_manager.users.views import UserFormCreateView, UsersIndexView
@@ -6,4 +7,5 @@ app_name = 'users'
 urlpatterns = [
     path('', UsersIndexView.as_view(), name='users'),
     path("create/", UserFormCreateView.as_view(), name="user_create"),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
