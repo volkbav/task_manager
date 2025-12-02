c800e10c7cc9cbdcde745dd6221ecd72900b173a
alex <berezov86@gmail.com>
2025-12-02 16:48:58 +0300

    add first app test

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -1,3 +1,11 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+# интеграционный тест
+class UserTest(TestCase):
+    # проверяем ответ от приложения users
+    def test_user_list(self):
+        response = self.client.get(reverse("users:users"))
+        self.assertEqual(response.status_code, 200)
