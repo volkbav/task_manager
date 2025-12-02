0d3c03fdd032b024892bd45de8579bdd7e6cc0c2
alex <berezov86@gmail.com>
2025-12-02 17:00:02 +0300

    update tests

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 
@@ -6,6 +7,15 @@ from django.urls import reverse
 # интеграционный тест
 class UserTest(TestCase):
     # проверяем ответ от приложения users
+
+    def setUp(self):
+        self.user = User.objects.create(
+            username="john",
+        first_name="John",
+        email="john@example.com",
+        password="123456")
+    
     def test_user_list(self):
         response = self.client.get(reverse("users:users"))
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "John")
