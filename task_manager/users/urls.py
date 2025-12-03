d32e81a08af39f84f88d1f3c25525ac21ea9f303
alex <berezov86@gmail.com>
2025-12-03 11:25:45 +0300

    bugfix

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -2,16 +2,21 @@ from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
 
 from task_manager.users.forms import UserFormLogin
-from task_manager.users.views import UserFormCreateView, UsersIndexView
+from task_manager.users.views import (
+    UserFormCreateView,
+    UsersIndexView,
+    MyLoginView,
+    MyLogoutView,
+)
 
 app_name = 'users'
 
 urlpatterns = [
     path('', UsersIndexView.as_view(), name='users'),
     path("create/", UserFormCreateView.as_view(), name="user_create"),
-    path('login/', LoginView.as_view(
+    path('login/', MyLoginView.as_view(
         template_name="registration/login.html",
         authentication_form=UserFormLogin,
     ), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', MyLogoutView.as_view(), name='logout'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,13 +1,16 @@
 
 # Create your views here.
 # from django.http import HttpResponse
-from django.contrib.auth import login
+from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import redirect, render
 from django.views import View
 from django.views.generic import ListView
-
+from django.utils.translation import gettext_lazy as _
 from .forms import UserFormCreate, UserFormLogin
+
+
 
 
 # path ''
@@ -27,8 +30,24 @@ class UserFormCreateView(View):
         form = UserFormCreate(request.POST)
         if form.is_valid():  # Если данные корректные, то сохраняем данные формы
             form.save()
-            return redirect('users:users')  # Редирект на указанный маршрут
+            messages.info(request, _("User created"))
+            return redirect('users:login')  # Редирект на указанный маршрут
         # Если данные некорректные, то возвращаем человека обратно 
         # на страницу с заполненной формой
+        messages.error(request, _("Login failed"))
         return render(request, 'users/create.html', {'form': form})
 
+class MyLoginView(LoginView):
+    form_class = UserFormLogin
+    template_name = "registration/login.html"
+
+    def form_valid(self, form):
+        messages.success(self.request, _("Logged in successfully"))
+        return super().form_valid(form)
+    
+class MyLogoutView(LogoutView):
+    next_page = 'users:login'
+
+    def dispatch(self, request, *args, **kwargs):
+        messages.info(request, _("You have logged out"))
+        return super().dispatch(request, *args, **kwargs)
