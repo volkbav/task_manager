0d12e3a18793ec97331b850f95f813886c7b00b2
alex <berezov86@gmail.com>
2025-12-05 11:30:34 +0300

    add tests for user

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -7,10 +7,11 @@ from django.urls import reverse
 class UserTest(TestCase):
     
     def setUp(self):
-        self.user = User.objects.create(
-            username="john",
+        self.user = User.objects.create_user(
             first_name="John",
-            email="john@example.com",
+            last_name="John",
+            username="Jonny",
+            password="password123"  # NOSONAR
         )
     
     # проверяем ответ от приложения users
@@ -18,3 +19,28 @@ class UserTest(TestCase):
         response = self.client.get(reverse("users:users"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "John")
+
+    def test_user_update_flow(self):
+        update_url = reverse(
+            "users:update",
+            kwargs={"pk": self.user.pk} 
+        )
+        list_url = reverse("users:users")
+
+        # Отправка POST-запроса на изменение
+        self.client.login(username="Jonny", password="password123")  # NOSONAR
+        self.client.post(
+            update_url,
+            data={
+                "username": "Bob",
+                "password1": "password123",  # NOSONAR
+                "password2": "password123",  # NOSONAR
+            }
+        )
+
+        # Переход на страницу списка пользователей
+        response = self.client.get(list_url)
+
+        # Проверяем, что новое имя отрисовано в HTML
+        self.assertContains(response, "Bob")
+        self.assertNotContains(response, "John")
